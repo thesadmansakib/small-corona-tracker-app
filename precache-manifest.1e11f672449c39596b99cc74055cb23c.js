a9eb3a3d01454abf943547e6f5f036ab17a21300
Sadman Sakib <sadman4523@gmail.com>
2020-07-23 06:00:01 +0600

    Updates

---
+++ precache-manifest.1e11f672449c39596b99cc74055cb23c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e935d02ac343962789accd3e35345f03",
+    "url": "/small-corona-tracker-app/index.html"
+  },
+  {
+    "revision": "cb8093a55207c3083aaf",
+    "url": "/small-corona-tracker-app/static/css/main.0ad28dd6.chunk.css"
+  },
+  {
+    "revision": "6823e0afbbb69d8ccf05",
+    "url": "/small-corona-tracker-app/static/js/2.eadb3ac4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/small-corona-tracker-app/static/js/2.eadb3ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8093a55207c3083aaf",
+    "url": "/small-corona-tracker-app/static/js/main.b9d1d8cf.chunk.js"
+  },
+  {
+    "revision": "49e5651c0b1eaef4c0ae",
+    "url": "/small-corona-tracker-app/static/js/runtime-main.9fab439a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/small-corona-tracker-app/static/media/image.d7265326.png"
+  }
+]);
